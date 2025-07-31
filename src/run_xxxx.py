f8ee5422970cf92d2d2b2012d9e12b3e1c56a8b3
tei <ymt.officemail@gmail.com>
2025-07-31 16:27:00 +0900

    fix:

--- src/run_xxxx.py
+++ src/run_xxxx.py
@@ -1,10 +1,13 @@
 # src/run_xxxx.py
 
+import logging
+
 from .libs.setInitialize import setInitialize
 
 
 def main():
     initData, logger = setInitialize()
+    logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
